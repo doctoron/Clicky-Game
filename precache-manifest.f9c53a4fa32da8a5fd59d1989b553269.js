a8345d83300b2a59b9cee05f207cb98e6dfe1a8c
doctoron <ron.antonio@me.com>
2019-04-16 20:49:37 -0400

    Updates

---
+++ precache-manifest.f9c53a4fa32da8a5fd59d1989b553269.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fab82da4d9420c34bc57",
+    "url": "/Clicky-Game/static/js/runtime~main.012bace8.js"
+  },
+  {
+    "revision": "9c56afd2b43a2b07141b",
+    "url": "/Clicky-Game/static/js/main.b9f4e22a.chunk.js"
+  },
+  {
+    "revision": "dd0ed99a380f8d225736",
+    "url": "/Clicky-Game/static/js/2.884c28c3.chunk.js"
+  },
+  {
+    "revision": "9c56afd2b43a2b07141b",
+    "url": "/Clicky-Game/static/css/main.173e4692.chunk.css"
+  },
+  {
+    "revision": "d0780c9e6858de9200908ee535323b25",
+    "url": "/Clicky-Game/index.html"
+  }
+];
